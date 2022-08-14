06105a97dae7d253ddb87c0be59b5573c03bad70
Mark Olson <markolsonse@icloud.com>
2022-08-14 10:13:48 +0200

    adding zotxt pkg

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -49,6 +49,7 @@
 (package! org-transclusion)
 (package! org-present)
 (package! visual-fill-column)
+(package! zotxt)
 ;; ...or multiple packages
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
