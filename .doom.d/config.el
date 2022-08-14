87d7f58ecf98cc6e955da15bc94115277fdbd238
Mark Olson <markolsonse@icloud.com>
2022-08-14 10:11:54 +0200

    update doom themes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -44,9 +44,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-one)
 ;;(setq doom-theme 'doom-palenight)
-(setq doom-theme 'doom-nord)
+;;(setq doom-theme 'doom-nord)
 
 ;; Let the desktop background show through
 ;;(set-frame-parameter (selected-frame) 'alpha '(97 . 100))
