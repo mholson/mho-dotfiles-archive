c2c876926515a747d6faded2c7e9ff3e5300ee04
Mark Olson <markolsonse@icloud.com>
2022-08-14 09:30:53 +0200

    changing path to org-roam

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -108,7 +108,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/")
-(setq org-roam-directory "~/Github/mhoOrgRoamGarden/")
+(setq org-roam-directory "~/Github/mhoOrgRoam/")
 
 ;; Load org-faces to make sure we can set appropriate faces
 (require 'org-faces)
