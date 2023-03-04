0f01824dcebc9068751f0a80ecc2d5870112d0a7
mholson <markolsonse@icloud.com>
2023-03-04 08:13:46 +0100

    typo

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,4 +1,4 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
