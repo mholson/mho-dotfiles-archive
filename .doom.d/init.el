22a23b139c9cca81862f0a7d548882794809e882
mholson <markolsonse@icloud.com>
2023-03-04 08:08:30 +0100

    including ibuffer

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -70,7 +70,7 @@
        (dired             ; making dired pretty [functional]
        +icons)            ; colorful icons for dired-mode
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
