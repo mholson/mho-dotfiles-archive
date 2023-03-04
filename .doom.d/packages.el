d2a46235be019394b75670143adf602bce30a78b
mholson <markolsonse@icloud.com>
2023-03-04 08:15:15 +0100

    calctex installed

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -51,6 +51,8 @@
 (package! org-super-agenda)
 (package! aas)
 (package! laas)
+(package! calctex :recipe (:host github :repo "johnbcoughlin/calctex"
+                           :files ("*.el" "calctex/*.el" "calctex-contrib/*.el" "org-calctex/*.el" "vendor")))
 ;; ...or multiple packages
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
