5b9df8c9a6e2f6b0d4ffcc6551d7358b96ba29c6
mholson <markolsonse@icloud.com>
2023-03-04 08:14:18 +0100

    fish update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /Users/markolson/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+ #>>> conda initialize >>>
+ # !! Contents within this block are managed by 'conda init' !!
+ eval ~/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+ # <<< conda initialize <<<
 # <<< conda initialize <<<
 set -g theme_color_scheme nord
+set -g theme_nerd_fonts yes
